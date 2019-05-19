041ff43cd1bf840cde5129989b36e19bffbe4737
Joel Oswald <joel@stefamon.de>
2019-05-19 15:12:54 +0200

    Createing Test Data

--- Apriori/apriory.py
+++ Apriori/apriory.py
@@ -2,10 +2,11 @@
 # [[[T-Nrt][Item 1, Item 2, Item 3]],[[T-3689], [1,2,2,3,8,7]]]
 #from powerset import potenzmenge
 from itertools import combinations, product
+from create_transactions import create_transactions
 
 def apriori (transactions, min_trashhold, max_iterations):
     c=[0]
-    c.append(count_items([a[1] for a in transactions], 2))
+    c.append(count_items([a[1] for a in transactions], 4))
     f=[0]
     f.append({k:v for k,v in c[1].items() if v >= min_trashhold})
     #for ind, candidates in enumerate(f):
@@ -13,7 +14,7 @@ def apriori (transactions, min_trashhold, max_iterations):
     
     iteration = 1
 
-    inner_join(transactions, f[1], 2)
+    inner_join(transactions, f[1], 4)
     
 
 
@@ -33,16 +34,21 @@ def count_items(itemsets, size):
 
 
 
-def inner_join(itemsets, counted, new_size):
-    c=dict()
+def inner_join(itemsets, counted, current_size):
+    c=[]
     for freq_itemset in combinations(counted.keys(),2):
-        print('Häufige Tupel, in Kombination miteinander: ', freq_itemset)
-        #if (freq_itemset[0][0:new_size] == freq_itemset[1][new_size]):
-            print(freq_itemset)
-
+       # print('Häufige Tupel, in Kombination miteinander: ', freq_itemset)
+        if (freq_itemset[0][0:current_size-1] == freq_itemset[1][0:current_size-1]):
+            # print('Juhu, Kandidat gefunden!: ', freq_itemset)
+            s=set(freq_itemset[0])
+            s.add(freq_itemset[1][current_size-1])
+            c.append(tuple(s))
+    
+    print('Übereinstimmende Itemsets: ', c)
 
 
 if __name__ == '__main__':
-    apriori([[[1], [1,2,3,4,5]], [[2],[2,3,4,5,6,7,8]], [[3], [7,8,9,10]]], 2,1000)
+    #apriori([[[1], [1,2,3,4,5]], [[2],[2,3,4,5,6,7,8]], [[3], [7,8,9,10]]], 2,1000)
+    apriori(create_transactions(200,10,10),3, 15)
 
   
